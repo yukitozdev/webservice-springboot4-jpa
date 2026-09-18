f9bd484ab1541ba3791c2257ecccfa26aa65f3f6
Matheus Costa <mcostavalentimribeiro@gmail.com>
2026-09-18 18:46:24 -0300

    Service layer, component registration

--- src/main/java/com/yukitoz/webservice/resources/UserResource.java
+++ src/main/java/com/yukitoz/webservice/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.yukitoz.webservice.resources;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yukitoz.webservice.entities.User;
+import com.yukitoz.webservice.services.UserService;
 
 @RestController 
 @RequestMapping (value = "/users")
 public class UserResource {
 
+    @Autowired 
+    private UserService service;
+
     @GetMapping 
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria" , "maria@gmail.com", "99999", "12345");
-        return ResponseEntity.ok(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok(obj);
     }
 
 }
